4b57e17b951c56af8d048ecba12721356e1c8b57
Adam Lis <adambricelis@gmail.com>
2019-02-26 16:52:50 -0500

    PT 2 bonus tweaks

--- FEH-Robot/main.cpp
+++ FEH-Robot/main.cpp
@@ -22,7 +22,29 @@ int main(void)
 
     // Plays Dance Dance Robot
     playDDR();
+
+    // Drives to ramp
+    driveStraightDistance(DEFAULT_MOTOR_PERCENT, FORWARD, 20.0);
+    turn(TURN_MOTOR_PERCENT, RIGHT, 49);
+    driveStraightDistance(DEFAULT_MOTOR_PERCENT, FORWARD, 5.0);
+    turn(TURN_MOTOR_PERCENT, RIGHT, 49);
+
+    // Drives up ramp
+    driveStraightDistance(RAMP_MOTOR_PERCENT, FORWARD, 32.0);
+    Sleep(0.5);
+
+    // Drives to foosball counters
+    driveArcDistance(FAST_MOTOR_PERCENT, -SLOW_MOTOR_PERCENT, FORWARD, 4.7);
+    setBreakpoint(1);
+    driveStraightDistance(DEFAULT_MOTOR_PERCENT, FORWARD, 7.0);
+    setBreakpoint(2);
+    turn(TURN_MOTOR_PERCENT, LEFT, 40);
+    setBreakpoint(3);
+    driveStraightDistance(DEFAULT_MOTOR_PERCENT, FORWARD, 10.0);
+    setBreakpoint(4);
+    turn(TURN_MOTOR_PERCENT, LEFT, 40);
     setBreakpoint(5);
+    driveStraightTime(DEFAULT_MOTOR_PERCENT, FORWARD, 2.25);
 
     // END PT 2 CODE
 
