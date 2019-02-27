9ccede7662c5b6292725ed071a00a82162fa0e95
jgeorge37 <george.921@osu.edu>
2019-02-27 12:01:22 -0500

    breakpoints added for tesitng

--- FEH-Robot/main.cpp
+++ FEH-Robot/main.cpp
@@ -16,23 +16,28 @@ int main(void)
 
     //drive to ramp
     turn(TURN_MOTOR_PERCENT, LEFT, 45);
-    driveStraightDistance(FAST_MOTOR_PERCENT, FORWARD, 16.0);   //16 was not an acutal measurement
+    driveStraightDistance(FAST_MOTOR_PERCENT, FORWARD, 16.0);   //16 was not an actual measurement
+    setBreakpoint(1);
 
     // Drives up ramp
     driveStraightDistance(RAMP_MOTOR_PERCENT, FORWARD, 16.0);   //need to measure
     Sleep(0.5);
+    setBreakpoint(2);
 
     //go to token machine
     turn(TURN_MOTOR_PERCENT, RIGHT, 90);
     driveStraightDistance(DEFAULT_MOTOR_PERCENT, FORWARD, 8.0); //need to measure
     turn(TURN_MOTOR_PERCENT, RIGHT, 90);
+    setBreakpoint(3);
 
     //drop token
     dropToken();
+    setBreakpoint(4);
 
     //go to lever
     turn(TURN_MOTOR_PERCENT, LEFT, 120);
     driveArcDistance(0.25, 0.50, FORWARD, 32.0);    //need to measure
+    setBreakpoint(5);
 
     //press lever
     flipLever();
