a4d2c8189fd871a4af6da48d8835ac2312613586
Adam Lis <adambricelis@gmail.com>
2019-04-02 16:42:48 -0400

    Light sensor hardware changes

--- FEH-Robot/objects_constants.h
+++ FEH-Robot/objects_constants.h
@@ -55,11 +55,10 @@ enum LightColor {
 };
 
 // Light color voltage values
-const float RED_LIGHT = 0.379;
-const float BLUE_LIGHT = 0.765;
-const float NO_LIGHT = 3.205;
+const float RED_LIGHT = 0.191;
+const float BLUE_LIGHT = 0.866;
 // Error bound for light measurement values
-const float LIGHT_EPSILON = 0.175;
+const float LIGHT_EPSILON = 0.335;
 
 /*
  * Microswitch constants
